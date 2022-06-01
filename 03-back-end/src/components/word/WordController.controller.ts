608532c36752723b7de5710bf147a7cf04ddac0c
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-06-01 14:32:03 +0200

    Mehanizam za transakcije na tabelu reči; Optimizovano hvatanje grešaka

--- 03-back-end/src/components/word/WordController.controller.ts
+++ 03-back-end/src/components/word/WordController.controller.ts
@@ -10,7 +10,9 @@ class WordController extends BaseController {
         res.send(result);
       })
       .catch((error) => {
-        res.status(500).send(error?.message);
+        setTimeout(() => {
+          res.status(500).send(error?.message);
+        }, 500);
       });
   }
 
@@ -21,13 +23,18 @@ class WordController extends BaseController {
       .getById(id, {})
       .then((result) => {
         if (result === null) {
-          return res.sendStatus(404);
+          throw {
+            status: 404,
+            message: "The word is not found!",
+          };
         }
 
         res.send(result);
       })
       .catch((error) => {
-        res.status(500).send(error?.message);
+        setTimeout(() => {
+          res.status(error?.status ?? 500).send(error?.message);
+        }, 500);
       });
   }
 
@@ -38,13 +45,18 @@ class WordController extends BaseController {
       .getByName(name)
       .then((result) => {
         if (result === null) {
-          return res.sendStatus(404);
+          throw {
+            status: 404,
+            message: "The word is not found!",
+          };
         }
 
         res.send(result);
       })
       .catch((error) => {
-        res.status(500).send(error?.message);
+        setTimeout(() => {
+          res.status(error?.status ?? 500).send(error?.message);
+        }, 500);
       });
   }
 
@@ -55,14 +67,18 @@ class WordController extends BaseController {
       return res.status(400).send(AddWordValidator.errors);
     }
 
-    this.services.word
-      .add(data)
-      .then((result) => {
-        res.send(result);
-      })
-      .catch((error) => {
-        res.status(400).send(error?.message);
-      });
+    this.services.word.startTransaction().then(() => {
+      this.services.word
+        .add(data)
+        .then(async (result) => {
+          await this.services.word.commitChanges();
+          res.send(result);
+        })
+        .catch(async (error) => {
+          await this.services.word.rollbackChanges();
+          res.status(400).send(error?.message);
+        });
+    });
   }
 
   edit(req: Request, res: Response) {
@@ -73,51 +89,69 @@ class WordController extends BaseController {
       return res.status(400).send(EditWordValidator.errors);
     }
 
-    this.services.word
-      .getById(id, {})
-      .then((result) => {
-        if (result === null) {
-          return res.sendStatus(404);
-        }
+    this.services.word.startTransaction().then(() => {
+      this.services.word
+        .getById(id, {})
+        .then((result) => {
+          if (result === null) {
+            throw {
+              status: 404,
+              message: "The word is not found!",
+            };
+          }
 
-        this.services.word
-          .editById(id, {
-            name: data.name,
-          })
-          .then((result) => {
-            res.send(result);
-          })
-          .catch((error) => {
-            res.status(400).send(error?.message);
-          });
-      })
-      .catch((error) => {
-        res.status(500).send(error?.message);
-      });
+          this.services.word
+            .editById(id, {
+              name: data.name,
+            })
+            .then(async (result) => {
+              await this.services.word.commitChanges();
+              res.send(result);
+            })
+            .catch(async (error) => {
+              await this.services.word.rollbackChanges();
+              res.status(400).send(error?.message);
+            });
+        })
+        .catch((error) => {
+          setTimeout(() => {
+            res.status(error?.status ?? 500).send(error?.message);
+          }, 500);
+        });
+    });
   }
 
   delete(req: Request, res: Response) {
     const id: number = +req.params?.wid;
 
-    this.services.word
-      .getById(id, {})
-      .then((result) => {
-        if (result === null) {
-          return res.sendStatus(404);
-        }
+    this.services.word.startTransaction().then(() => {
+      this.services.word
+        .getById(id, {})
+        .then((result) => {
+          if (result === null) {
+            throw {
+              status: 404,
+              message: "The word is not found!",
+            };
+          }
 
-        this.services.word
-          .deleteById(id)
-          .then((_result) => {
-            res.send("This word has been deleted!");
-          })
-          .catch((error) => {
-            res.status(500).send(error?.message);
-          });
-      })
-      .catch((error) => {
-        res.status(500).send(error?.message);
-      });
+          this.services.word
+            .deleteById(id)
+            .then(async (_result) => {
+              await this.services.word.commitChanges();
+              res.send("This word has been deleted!");
+            })
+            .catch(async (error) => {
+              await this.services.word.rollbackChanges();
+              res.status(500).send(error?.message);
+            });
+        })
+        .catch((error) => {
+          setTimeout(() => {
+            res.status(error?.status ?? 500).send(error?.message);
+          }, 500);
+        });
+    });
   }
 }
 
