1c313d953b7db81b55eecc409a4a515f837c194c
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-06-30 21:25:50 +0200

    Dodate rute sa odrađenim React elementima do sada

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -4,16 +4,45 @@ import LoginPage from '../User/UserLoginPage/UserLoginPage';
 import ContactPage from '../Pages/ContactPage/ContactPage';
 import { Routes, Route } from 'react-router-dom';
 import Menu from '../Menu/Menu';
+import UserLoginPage from '../User/UserLoginPage/UserLoginPage';
+import AdministratorLoginPage from '../Administrator/AdministratorLoginPage/AdministratorLoginPage';
+import AdminDashboard from '../Administrator/Dashboard/AdminDashboard';
+import AdminQuestionList from '../Administrator/Dashboard/AdminQuestionList';
+import AdminUserList from '../Administrator/Dashboard/AdminUserList';
+import AdminAdministratorList from '../Administrator/Dashboard/AdminAdministratorList';
+import AdminAdministratorAdd from '../Administrator/Dashboard/AdminAdministratorAdd';
+import AdminAnswerList from '../Administrator/Dashboard/AdminAnswerList';
 
 function Application() {
   return (
     <Container className="mt-4">
       <Menu />
-        <Routes>
-          <Route path='/' element={<div></div>} />
-          <Route path='/contact' element={<ContactPage />} />
-          <Route path='/auth/user/login' element={<LoginPage />} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<div></div>} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/auth/user/login' element={<UserLoginPage />} />
+        {/* TODO <Route path='/auth/user/register' element={<UserRegisterPage />} /> */}
+        <Route path='/auth/administrator/login' element={<AdministratorLoginPage />} />
+        {/* TODO <Route path="/questions" element={<UserQuestionList />} /> */}
+        {/* TODO <Route path="/question/:id" element={<UserQuestionPage />} /> */}
+
+        {/* TODO <Route path="/profile" element={<UserProfile />} /> */}
+
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/dashboard/question/list" element={<AdminQuestionList />} />
+        {/* TODO <Route path="/admin/dashboard/question/:qid/answers/list" element={<AdminQuestionAnswerList />} />
+        TODO <Route path="/admin/dashboard/question/:qid/answers/add" element={<AdminQuestionAnswerAdd />} />
+        TODO <Route path="/admin/dashboard/question/:qid/answers/edit/:aid" element={<AdminQuestionAnswerEdit />} /> */}
+
+        <Route path='/admin/dashboard/answer/list' element={<AdminAnswerList />} />
+        {/* TODO <Route path='/admin/dashboard/answer/add' element={<AdminAnswerAdd />} />
+        TODO <Route path='/admin/dashboard/answer/list' element={<AdminAnswerEdit />} /> */}
+
+        <Route path="/admin/dashboard/administrator/list" element={<AdminAdministratorList />} />
+        <Route path="/admin/dashboard/administrator/add" element={<AdminAdministratorAdd />} />
+
+        <Route path="/admin/dashboard/user/list" element={<AdminUserList />} />
+      </Routes>
     </Container>
   );
 }
