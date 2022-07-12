0efb631a810abd6b9808d77126eaef4670dc5704
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-07-13 00:53:29 +0200

    Formatiranje stranice

--- 04-front-end/src/components/Administrator/AdministratorLoginPage/AdministratorLoginPage.tsx
+++ 04-front-end/src/components/Administrator/AdministratorLoginPage/AdministratorLoginPage.tsx
@@ -5,39 +5,39 @@ import AppStore from "../../../stores/AppStore";
 import { motion } from "framer-motion";
 
 export default function AdministratorLoginPage() {
-    const [ username, setUsername ] = useState<string>("");
-    const [ password, setPassword ] = useState<string>("");
-    const [ error, setError ] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [error, setError] = useState<string>("");
 
     const navigate = useNavigate();
 
     const doLogin = () => {
         api("post", "/api/auth/administrator/login", "administrator", { username, password })
-        .then(res => {
-            if (res.status !== "ok") {
-                throw new Error("Could not log in. Reason: " + JSON.stringify(res.data));
-            }
+            .then(res => {
+                if (res.status !== "ok") {
+                    throw new Error("Could not log in. Reason: " + JSON.stringify(res.data));
+                }
 
-            return res.data;
-        })
-        .then(data => {
-            AppStore.dispatch( { type: "auth.update", key: "authToken", value: data?.authToken } );
-            AppStore.dispatch( { type: "auth.update", key: "refreshToken", value: data?.refreshToken } );
-            AppStore.dispatch( { type: "auth.update", key: "identity", value: username } );
-            AppStore.dispatch( { type: "auth.update", key: "id", value: +(data?.id) } );
-            AppStore.dispatch( { type: "auth.update", key: "role", value: "administrator" } );
+                return res.data;
+            })
+            .then(data => {
+                AppStore.dispatch({ type: "auth.update", key: "authToken", value: data?.authToken });
+                AppStore.dispatch({ type: "auth.update", key: "refreshToken", value: data?.refreshToken });
+                AppStore.dispatch({ type: "auth.update", key: "identity", value: username });
+                AppStore.dispatch({ type: "auth.update", key: "id", value: +(data?.id) });
+                AppStore.dispatch({ type: "auth.update", key: "role", value: "administrator" });
 
-            navigate("/admin/dashboard", {
-                replace: true,
+                navigate("/admin/dashboard", {
+                    replace: true,
+                });
+            })
+            .catch(error => {
+                setError(error?.message ?? "Could not log in!");
+
+                setTimeout(() => {
+                    setError("");
+                }, 3500);
             });
-        })
-        .catch(error => {
-            setError(error?.message ?? "Could not log in!");
-
-            setTimeout(() => {
-                setError("");
-            }, 3500);
-        });
     };
 
     return (
@@ -62,25 +62,25 @@ export default function AdministratorLoginPage() {
 
                 <div className="form-group mb-3">
                     <div className="input-group">
-                        <input className="form-control" type="text" placeholder="Enter your username" value={ username }
-                               onChange={ e => setUsername(e.target.value) } />
+                        <input className="form-control" type="text" placeholder="Enter your username" value={username}
+                            onChange={e => setUsername(e.target.value)} />
                     </div>
                 </div>
 
                 <div className="form-group mb-3">
                     <div className="input-group">
-                        <input className="form-control" type="password" placeholder="Enter your password" value={ password }
-                               onChange={ e => setPassword(e.target.value) }/>
+                        <input className="form-control" type="password" placeholder="Enter your password" value={password}
+                            onChange={e => setPassword(e.target.value)} />
                     </div>
                 </div>
 
                 <div className="form-group mb-3">
-                    <button className="btn btn-primary px-5" onClick={ () => doLogin() }>
+                    <button className="btn btn-primary px-5" onClick={() => doLogin()}>
                         Log in
                     </button>
                 </div>
 
-                { error && <p className="alert alert-danger">{ error }</p> }
+                {error && <p className="alert alert-danger">{error}</p>}
             </div>
         </motion.div>
     );
