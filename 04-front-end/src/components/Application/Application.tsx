eaf3e357ec7d04c86779a7c3018272454518dcec
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-07-12 19:05:27 +0200

    Restrukturiranje stranica za prikaz kroz ruter; Vraćene dodate stranice iz komentara

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import LoginPage from '../User/UserLoginPage/UserLoginPage';
 import ContactPage from '../Pages/ContactPage/ContactPage';
 import { Routes, Route } from 'react-router-dom';
 import Menu from '../Menu/Menu';
@@ -15,6 +14,8 @@ import AdminAnswerList from '../Administrator/Dashboard/AdminAnswerList';
 import AdminQuestionAnswerList from '../Administrator/Dashboard/AdminQuestionAnswerList';
 import AdminQuestionAdd from '../Administrator/Dashboard/AdminQuestionAdd';
 import AdminQuestionEdit from '../Administrator/Dashboard/AdminQuestionEdit';
+import UserRegisterPage from '../User/UserRegisterPage/UserRegisterPage';
+import QuizPage from '../Pages/QuizPage/QuizPage';
 
 function Application() {
   return (
@@ -23,25 +24,32 @@ function Application() {
       <Routes>
         <Route path='/' element={<div></div>} />
         <Route path='/contact' element={<ContactPage />} />
-        <Route path='/auth/user/login' element={<UserLoginPage />} />
-        {/* TODO <Route path='/auth/user/register' element={<UserRegisterPage />} /> */}
+
         <Route path='/auth/administrator/login' element={<AdministratorLoginPage />} />
+        <Route path='/auth/user/login' element={<UserLoginPage />} />
+        <Route path='/auth/user/register' element={<UserRegisterPage />} />
+
         {/* TODO <Route path="/questions" element={<UserQuestionList />} /> */}
-        {/* TODO <Route path="/question/:id" element={<UserQuestionPage />} /> */}
+        {/* TODO <Route path="/question/add" element={<UserQuestionAdd />} /> */}
+        {/* TODO <Route path="/question/:qid/edit" element={<UserQuestionEdit />} /> */}
+        {/* TODO <Route path="/question/:qid/answers/list" element={<UserQuestionAnswerList />} /> */}
 
         {/* TODO <Route path="/profile" element={<UserProfile />} /> */}
+        {/* TODO <Route path="/myscores" element={<UserScoresPage />} /> */}
+
+        <Route path="/quiz" element={<QuizPage />} />
+
+        {/* TODO <Route path="/scores" element={<HighScoresPage />} /> */}
 
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/dashboard/question/list" element={<AdminQuestionList />} />
         <Route path="/admin/dashboard/question/add" element={<AdminQuestionAdd />} />
         <Route path="/admin/dashboard/question/:qid/edit" element={<AdminQuestionEdit />} />
         <Route path="/admin/dashboard/question/:qid/answers/list" element={<AdminQuestionAnswerList />} />
-        {/* TODO <Route path="/admin/dashboard/question/:qid/answers/add" element={<AdminQuestionAnswerAdd />} />
-        TODO <Route path="/admin/dashboard/question/:qid/answers/edit/:aid" element={<AdminQuestionAnswerEdit />} /> */}
 
         <Route path='/admin/dashboard/answer/list' element={<AdminAnswerList />} />
         {/* TODO <Route path='/admin/dashboard/answer/add' element={<AdminAnswerAdd />} />
-        TODO <Route path='/admin/dashboard/answer/list' element={<AdminAnswerEdit />} /> */}
+        TODO <Route path='/admin/dashboard/answer/edit' element={<AdminAnswerEdit />} /> */}
 
         <Route path="/admin/dashboard/administrator/list" element={<AdminAdministratorList />} />
         <Route path="/admin/dashboard/administrator/add" element={<AdminAdministratorAdd />} />
