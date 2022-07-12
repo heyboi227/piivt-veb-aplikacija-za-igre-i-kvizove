e473ade948f9b6043c8e23b15a82c1b79cd19ddb
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-07-12 19:08:08 +0200

    Dijalog za potvrđivanje akcije/prenos podataka

---
+++ 04-front-end/src/helpers/SubmitAction.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+export interface ISubmitActionProperties {
+    title: string;
+    message: string;
+    username: string;
+    setUsername: (value: string) => void;
+    onSubmit: () => void;
+    onCancel: () => void;
+}
+
+export default function SubmitAction(props: ISubmitActionProperties) {
+
+    return (
+        <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={true}>
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {props.title}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>{props.message}</p>
+                <div className="form-group mb-3">
+                    <div className="input-group">
+                        <input className="form-control" type="text" placeholder="Enter your username" value={props.username}
+                            onChange={e => props.setUsername(e.target.value)} />
+                    </div>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button className="btn btn-lg btn-success" onClick={props.onSubmit}>Submit</Button>
+                <Button className="btn btn-lg btn-danger" onClick={props.onCancel}>Cancel</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
