8bdaf24f1253bcae29e1c9ada40a1fd0ea09e982
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-07-25 10:51:47 +0200

    Komponenta za prikaz i izmenu parametara korisnika

---
+++ 04-front-end/src/components/User/Profile/UserProfile.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { api } from "../../../api/api";
+import IUser from "../../../models/IUser.model";
+import AppStore from "../../../stores/AppStore";
+import UserDetailsEditor from "./UserDetailsEditor";
+import UserPasswordChanger from "./UserPasswordChanger";
+
+export default function UserProfile() {
+    const [user, setUser] = useState<IUser>();
+
+    function loadUserData() {
+        if (AppStore.getState().auth.role !== "activeUser") {
+            return;
+        }
+
+        api("get", "/api/user/" + AppStore.getState().auth.id, "activeUser")
+            .then(res => {
+                if (res.status !== 'ok') {
+                    throw new Error("Coudl not fetch this data. Reason: " + JSON.stringify(res.data));
+                }
+
+                return res.data;
+            })
+            .then(user => {
+                setUser(user);
+            })
+            .catch(() => { });
+    };
+
+    useEffect(loadUserData, []);
+
+    if (AppStore.getState().auth.role !== "activeUser") {
+        return null;
+    }
+
+    return (
+        <div className="card">
+            <div className="card-body">
+                <div className="card-title mb-3">
+                    <h1 className="h5">My profile</h1>
+                </div>
+                <div className="card-text">
+                    <div className="row mb-4">
+                        <div className="col col-12 col-lg-6">
+                            {user && <UserDetailsEditor user={user} onDataChanged={user => setUser(user)} />}
+                        </div>
+
+                        <div className="col col-12 col-lg-6">
+                            {user && <UserPasswordChanger user={user} onPasswordChange={user => setUser(user)} />}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
