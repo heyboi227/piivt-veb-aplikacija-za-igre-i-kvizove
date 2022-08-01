f8e2db4e0d643168731c59fa62bea23c8b843ee8
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-08-01 13:04:32 +0200

    Prikaz korisnikovih pitanja na profile stranici

--- 04-front-end/src/components/User/Profile/UserProfile.tsx
+++ 04-front-end/src/components/User/Profile/UserProfile.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { api } from "../../../api/api";
 import IUser from "../../../models/IUser.model";
 import AppStore from "../../../stores/AppStore";
@@ -34,23 +35,38 @@ export default function UserProfile() {
     }
 
     return (
-        <div className="card">
-            <div className="card-body">
-                <div className="card-title mb-3">
-                    <h1 className="h5">My profile</h1>
-                </div>
-                <div className="card-text">
-                    <div className="row mb-4">
-                        <div className="col col-12 col-lg-6">
-                            {user && <UserDetailsEditor user={user} onDataChanged={user => setUser(user)} />}
-                        </div>
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <div className="card-title mb-3">
+                        <h1 className="h5">My profile</h1>
+                    </div>
+                    <div className="card-text">
+                        <div className="row mb-4">
+                            <div className="col col-12 col-lg-6">
+                                {user && <UserDetailsEditor user={user} onDataChanged={user => setUser(user)} />}
+                            </div>
 
-                        <div className="col col-12 col-lg-6">
-                            {user && <UserPasswordChanger user={user} onPasswordChange={user => setUser(user)} />}
+                            <div className="col col-12 col-lg-6">
+                                {user && <UserPasswordChanger user={user} onPasswordChange={user => setUser(user)} />}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className="col-12 col-lg-4 col-md-6 col-xl-3 p-3">
+                <div className="card">
+                    <div className="card-body">
+                        <div className="card-title">
+                            <h2 className="h5">My questions</h2>
+                        </div>
+                        <div className="card-text d-grid gap-3">
+                            <Link className="btn btn-primary" to="/question">List all</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
